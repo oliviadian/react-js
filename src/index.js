433d20c76cc8f737b66561ed66d761a64e5326f8
oliviadian <oliviadiaan12@gmail.com>
2019-04-16 09:05:00 +0700

    reactjs

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+/*
+function formatNama(mahasiswa){
+    return mahasiswa.nama_depan + ' ' + mahasiswa.nama_belakang;
+}
+
+const mhs = {
+    nama_depan : 'Olivia',
+    nama_belakang : 'Dian'
+}
+
+//const nama = 'olive';
+const element = <h1> Hallo, namaku {formatNama(mhs)} </h1>;
+
+ReactDOM.render(element, document.getElementById('root'));
+
+
+function tick() {
+    const element = (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      </div>
+    );
+    ReactDOM.render(element, document.getElementById('root'));
+  }
+  
+  setInterval(tick, 1000);
+*/
+
+function HelloWorld(props){
+    return <h1>Hello {props.nama} from Component (function) </h1>
+}
+
+ReactDOM.render(<HelloWorld nama="Olivia"/>, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
